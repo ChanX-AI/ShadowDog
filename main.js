fd490a9180278e9f3d7aa644c4a5c848d8eaad25
ChanX <107697626+ChanX-AI@users.noreply.github.com>
2026-06-07 14:05:06 +0530

    Update main.js
    reverting back to normal ms

--- main.js
+++ main.js
@@ -340,7 +340,7 @@ window.addEventListener('load', function(){
     requestAnimationFrame(animate);
     function animate(currTime) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        const deltaTime = (currTime - lastTime) / 1000;
+        const deltaTime = currTime - lastTime;
         lastTime = currTime;
         game.render(deltaTime);
         requestAnimationFrame(animate);
