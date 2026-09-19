89e505f60a686fb5058304428f19060e1185751e
ChanX <107697626+ChanX-AI@users.noreply.github.com>
2026-09-19 09:15:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ class Game {
 
         this.currentBG.forEach(bgImg => {
             bgImg.draw(ctx);
-            bgImg.update(this.gameSpeed);
+            bgImg.update(60 * this.deltaTime * this.gameSpeed);
         });
         
         this.#handleEnemies(deltaTime);
